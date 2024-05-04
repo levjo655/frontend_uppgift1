eece27136c492b239f58c63cdf354c31b5438265
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-05-04 14:03:24 +0200

    desktop version done

--- booking.js
+++ booking.js
@@ -1,10 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const bookingForm = document.querySelector('form');
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+function saveSeats() {
+  let seats = [];
+  const checkBoxes = document.querySelectorAll(
+    "input[type='checkbox']:checked"
+  );
+  for (let i = 0; i < checkBoxes.length; i++) {
+    seats.push(checkBoxes[i].id);
+  }
 
-    if (selectedSeats) {
-        const bookingInfoDiv = document.createElement('div');
-        bookingInfoDiv.innerHTML = `<p>Selected Seats: ${selectedSeats.join(', ')}</p>`;
-        bookingForm.prepend(bookingInfoDiv);
-    }
+  localStorage.setItem("selectedSeats", JSON.stringify(seats));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const getBoxes = JSON.parse(localStorage.getItem("selectedSeats"));
+  const ul = document.getElementById("selected-seats-list");
+  getBoxes.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerHTML = element;
+    ul.appendChild(li);
+  });
 });
+
+function confirmation() {
+  alert("Booking is complete, check your email inbox");
+}
+
+// if (selectedSeats) {
+//     const bookingInfoDiv = document.createElement('div');
+//     bookingInfoDiv.innerHTML = `<p>Selected Seats: ${selectedSeats.join(', ')}</p>`;
+//     bookingForm.prepend(bookingInfoDiv);
+// }
